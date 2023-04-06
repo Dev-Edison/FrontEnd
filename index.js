502d466af45538d9ae36f00b0f28929e36c26f39
Dev-Edison <dev3dison@gmail.com>
2023-04-05 21:32:55 -0300

    2023.04.05

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ $(document).ready(myApp)
 
 function myApp() {
 
-    console.log('Funciona!')
+    // Carrega a págine inicial.
+    loadpage('user')
 
+}
+
+// Carrega uma página.
+// O caminho final de página é passado como parâmentro da função
+function loadpage(page) {
+
+
+
+    // Monta os caminhos para os componentes da página solicitadas
+    const path = {
+        html: `/pages/${page}/index.html`,
+        css: `/pages/${page}/index.css`,
+        js: `/pages/${page}/index.js`,
+
+    }
+
+    $.get(path.html)
+        .done((data) => {
+            $('main').html(data)
+            })
+            .fail((error) => {
+             loadpage('e404')
+})
 }
