ef80262295b0a2328cd63b321fa235134fd9983a
Dev-Edison <dev3dison@gmail.com>
2023-04-20 21:42:36 -0300

    2023.04.20(SQL)

--- api/FrontEnd.sql
+++ api/FrontEnd.sql
@@ -1,24 +1,23 @@
--- ----------------------------------------------
--- FrontEnd - Banco de dados
+-- ---------------------------------
+-- FrontEndeiros - Banco de dados
 -- By Edison
 -- MIT License
---
--- Modela o banco de dados da api do aplicativo.
--- ---------------------------------------------
+-- 
+-- Modela o banco de dados da API do aplicativo.
+-- ---------------------------------
 
 -- Apaga o banco de dados caso ele exista.
 -- IMPORTANTE! Só faça isso em momento de desenvolvimento.
--- Nunca user este código em produção.
+-- Nunca use este código em produção.
+DROP DATABASE IF EXISTS frontendeiros;
 
- DROP DATABASE IF EXISTS FrontEnd;
+-- Cria o banco de dados com caracteres utf8 e buscas case-insensitive.
+CREATE DATABASE frontendeiros CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
- -- Cria o banco de dados com caracteres e buscas cae-inesensitive.
- CREATE DATABASE FrontEnd  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- Seleciona o banco de dados para as próximas interações.
+USE frontendeiros;
 
- -- Seleciona o banco de dados para as proximas interações.
- USE FrontEnd;
-
- -- Cria tabela dos contatos → contacts.
+-- Cria tabela dos contatos → contacts.
 CREATE TABLE contacts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -29,5 +28,144 @@ CREATE TABLE contacts (
     status ENUM ('received', 'readed', 'responded', 'deleted') DEFAULT 'received'
 );
 
+-- Cria a tabela de usuários → users.
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_photo VARCHAR(255) COMMENT 'URL da imagem.',
+    user_bio VARCHAR(255),
+    user_birth DATE,
+    user_type ENUM('user', 'moderator', 'author', 'admin') DEFAULT 'user',
+    user_status ENUM('on', 'off', 'ban', 'del') DEFAULT 'on'
+);
 
+-- Cria a tabela de artigos → articles.
+CREATE TABLE articles (
+    art_id INT PRIMARY KEY AUTO_INCREMENT,
+    art_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    art_author INT NOT NULL,
+    art_title VARCHAR(127) NOT NULL,
+    art_thumbnail VARCHAR(255) COMMENT 'URL da imagem.',
+    art_resume VARCHAR(127) NOT NULL,
+    art_content TEXT NOT NULL,
+    art_views INT DEFAULT 0,
+    art_status ENUM('on', 'off', 'del') DEFAULT 'on',
+    FOREIGN KEY (art_author) REFERENCES users (user_id)
+);
+
+-- Cria a tabela de comentários → comments.
+CREATE TABLE comments (
+    cmt_id INT PRIMARY KEY AUTO_INCREMENT,
+    cmt_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    cmt_author INT NOT NULL,
+    cmt_article INT NOT NULL,
+    cmt_comment TEXT NOT NULL,
+    cmt_status ENUM('on', 'off', 'del') DEFAULT 'on',
+    FOREIGN KEY (cmt_author) REFERENCES users (user_id),
+    FOREIGN KEY (cmt_article) REFERENCES articles (art_id)
+);
+
+-- Lista de redes sociais dos usuários → social.
+CREATE TABLE social (
+    scl_id INT PRIMARY KEY AUTO_INCREMENT,
+    scl_user INT NOT NULL,
+    scl_name VARCHAR(127) NOT NULL,
+    scl_link VARCHAR(255) NOT NULL,
+    scl_status ENUM('on', 'off', 'del') DEFAULT 'on',
+    FOREIGN KEY (scl_user) REFERENCES users (user_id) 
+);
+
+-- ---- --
+-- CRUD --
+-- ---- --
+
+-- Insere vários registros na tabela 'contacts'.
+INSERT INTO contacts 
+    ( name, email, subject, message )
+VALUES 
+    ('Joca da Silva', 'joca@silva.com', 'Erro', 'Não consigo me cadastrar.'),
+    ('Setembrino', 'set@brino.com.br', 'biscoito', 'Biscoit não é bolacha.'),
+    ('Maricleuza', 'mari@cleuza.net', 'bolacha', 'Bolacha é biscoito são a mesma coisa.');
+
+-- Insere dados na tabela 'users'.
+INSERT INTO users (
+    user_name,
+    user_email,
+    user_photo,
+    user_bio,
+    user_birth
+) VALUES (
+    'Joca da Silva',
+    'joca@silva.com',
+    'https://randomuser.me/api/portraits/men/33.jpg',
+    'Construtor, programador e enrolador',
+    '2000-10-28'
+);
+
+-- Insere dados na tabela 'users'.
+-- Somente campos "not null".
+INSERT INTO users (
+    user_name,
+    user_email
+) VALUES (
+    'Setembrino',
+    'set@brino.com.br'
+);
+
+-- Insere dados na tabela 'users'.
+-- Somente o 'name'.
+INSERT INTO users (
+    user_name
+) VALUES (
+    'Hermenilda'
+);
+
+-- Insere artigos na tabela 'articles'.
+INSERT INTO articles (
+    art_author,
+    art_title,
+    art_thumbnail,
+    art_resume,
+    art_content
+) VALUES (
+    '1',
+    'Primeiro artigo da parada',
+    'https://picsum.photos/200',
+    'Este é o primeiro artigo do nosso blog.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a ultrices leo. Vivamus in suscipit quam. Sed posuere erat non massa vehicula laoreet.'
+);
+
+-- Insere artigos na tabela 'articles'.
+INSERT INTO articles (
+    art_author,
+    art_title,
+    art_thumbnail,
+    art_resume,
+    art_content
+) VALUES (
+    '1',
+    'Segundo artigo da parada',
+    'https://picsum.photos/201',
+    'Este é o segundo artigo do nosso blog.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a ultrices leo. Vivamus in suscipit quam. Sed posuere erat non massa vehicula laoreet.'
+);
+
+
+
+-- Atividade
+-- 1) Insira pelo menos mais 2 artigos.
+-- 2) Insira pelo menos 2 comentários de usuários diferentes para um mesmo artigo.
+
+
+cmt_id: Integer
+cmt_date: Datetime
+cmt_author: Integer
+cmt_article: Integer
+cmt_comment: Text
+cmt_status: List('on'*, 'off', 'del')
+cmt_id - Primary Key (PK)
+cmt_author - Foreign Key (FK) → users(user_id)
+cmt_article - Foreign Key (FK) → artciles(art_id)
 
